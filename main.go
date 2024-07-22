8128f8c7dee7055dc371ac64356e7d736ea9e96b
junta <juntakoman123@gmail.com>
2024-07-22 22:04:55 +0900

    一旦main関数にすべての記述を移動

--- main.go
+++ main.go
@@ -2,30 +2,75 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/juntakoman123/gin_todo_app/config"
-	"github.com/juntakoman123/gin_todo_app/controller"
-	"github.com/juntakoman123/gin_todo_app/model"
-	"github.com/juntakoman123/gin_todo_app/repository"
-	"github.com/juntakoman123/gin_todo_app/service"
 )
+
+type TaskID int64
+type TaskStatus string
+
+const (
+	TaskStatusTodo  TaskStatus = "todo"
+	TaskStatusDoing TaskStatus = "doing"
+	TaskStatusDone  TaskStatus = "done"
+)
+
+type Task struct {
+	ID      TaskID     `json:"id"`
+	Title   string     `json:"title"`
+	Status  TaskStatus `json:"status"`
+	Created time.Time  `json:"created"`
+}
+
+type Tasks []*Task
+
+type TaskController struct {
+	TaskService TaskService
+}
+
+func (tc *TaskController) GetTasks(c *gin.Context) {
+	tasks := tc.TaskService.GetTasks()
+	c.JSON(http.StatusOK, tasks)
+}
+
+type TaskService struct {
+	TaskRepo TaskRepository
+}
+
+func (ts *TaskService) GetTasks() Tasks {
+	return ts.TaskRepo.GetTasks()
+}
+
+type TaskRepository interface {
+	GetTasks() Tasks
+}
+
+type InMemoryTaskRepository struct {
+	Tasks Tasks
+}
+
+func (repo *InMemoryTaskRepository) GetTasks() Tasks {
+	return repo.Tasks
+}
 
 func main() {
 
-	taskRepo := &repository.InMemoryTaskRepository{
-		Tasks: model.Tasks{
-			{ID: 1, Title: "Task 1", Status: model.TaskStatusTodo},
-			{ID: 2, Title: "Task 2", Status: model.TaskStatusTodo},
+	taskRepo := &InMemoryTaskRepository{
+		Tasks: Tasks{
+			{ID: 1, Title: "Task 1", Status: TaskStatusTodo},
+			{ID: 2, Title: "Task 2", Status: TaskStatusTodo},
 		},
 	}
 
-	taskService := service.TaskService{
+	taskService := TaskService{
 		TaskRepo: taskRepo,
 	}
 
-	taskController := &controller.TaskController{
+	taskController := &TaskController{
 		TaskService: taskService,
 	}
 
